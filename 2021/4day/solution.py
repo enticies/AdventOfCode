6ec78bc462b32ad1c56df2130eaee41a5389e4f5
enticies <lasauthr@protonmail.com>
2021-12-16 07:20:17 +0400

    Day 4, Part 1 rewritten.

--- 2021/4day/solution.py
+++ 2021/4day/solution.py
@@ -1,44 +1,76 @@
-boards = []
+def main():
+    boards = []
 
-
-with open("input", "r") as file:
-    drawn = list(map(int, file.readline()[:-1:].split(",")))
-    line = file.readline()
-    board = []
-    while line:
-        if line == "\n" and board:
-            boards.append(board)
-            board = []
+    with open("input", "r") as file:
+        draws = list(map(int, file.readline()[:-1:].split(",")))
         line = file.readline()
-        if line != "\n":
-            column_string = line.replace("\n", "")
-            column = list(map(int, column_string.split()))
-            board.append(column)
+        board = []
+        while line:
+            if line == "\n" and board:
+                boards.append(board)
+                board = []
+            line = file.readline()
+            if line != "\n":
+                column_string = line.replace("\n", "")
+                column = list(map(int, column_string.split()))
+                board.append(column)
+
+    return boards, draws
 
 
+def get_cols(array):
+    cols = []
+
+    for i in range(len(array[0])):
+        t = []
+        for a in array:
+            t.append(a[i])
+        cols.append(t)
+
+    return cols
+
+def check_board(board, draws):
+    cols = get_cols(board)
+    
+    for col in cols:
+        if contained(draws, col):
+            return True
+
+    for row in board:
+        if contained(draws, row):
+            return True
+    return False
+    
 def contained(a1, a2):
     for i in a2:
         if i not in a1:
             return False
     return True
 
-d = []
+def calculate_board(board, draws):
+    t = 0
+    for row in board:
+        for item in row:
+            if item not in draws:
+                t += item
 
-for draw in drawn:
-    for board in boards:
-        for row in board:
-            if contained(d, row):
-                t = []
-                for i in board:
-                    for a in i:
-                        if a not in d:
-                            t.append(a)
-                print(sum(t) * d[-1])
-                exit()
-    d.append(draw)
+    return t * draws[-1]
 
 
 
+def find_winner(boards, draws):
+    d = []
+    for draw in draws:
+        for board in boards:
+            if check_board(board, d):
+                return calculate_board(board, d)
+        d.append(draw)
 
-for i in boards:
-    print(i)
+    return None
+
+if __name__ == "__main__":
+    boards, draws = main()
+
+    score = find_winner(boards, draws)
+    print(score)
+
