abb79778f26767ff7876408d626d3430a4304542
enticies <lasauthr@protonmail.com>
2021-12-23 16:51:31 +0400

    Day 6, Part 2.

--- 2021/6day/solution.py
+++ 2021/6day/solution.py
@@ -4,22 +4,34 @@ def parse_file():
 
     return ints
 
-def fun(ints):
-    for i in range(80):
-        t = []
-        for j in range(len(ints)):
-            if ints[j] == 0:
-                t.extend([6, 8])
-            else:
-                t.append(ints[j] - 1)
-        ints = t
+def fun(ints, days):
+    fish = dict()
 
-    return ints
+    for i in range(9):
+        if i in ints:
+            fish[i] = ints.count(i)
+        else:
+            fish[i] = 0
 
+    for i in range(days):
+        cpy = dict(fish)
+        day0 = fish[0]
+        fish[0] = 0
+        for a in range(8, 0, -1):
+            cpy[a - 1] = fish[a]
+        cpy[8] = day0
+        cpy[6] += day0
+
+        fish = cpy
+
+
+    return fish.values()
 
 
 if __name__=="__main__":
     ints = parse_file()
-    result = fun(ints)
+    result = fun(ints, 256)
 
-    print(len(result))
+
+    print(result)
+    print(sum(result))
