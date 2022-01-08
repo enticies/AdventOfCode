e59c03188d2a03951306cd74f5d4d8d8e660a6dc
enticies <lasauthr@protonmail.com>
2022-01-08 22:57:48 +0100

    Day 8, Part 2. I am dead.

--- 2021/8day/solution.py
+++ 2021/8day/solution.py
@@ -10,129 +10,106 @@ def get_input():
             line = file.readline()
     return ret
 
-def first_pass(line):
+def get_unique(line):
     h = {2: 1, 4: 4, 3: 7, 7: 8}
     unique = dict()
-    notunique = [] 
-    for value in line:
+    for value in line[0].split():
         if len(value) in h:
             unique[h[len(value)]] = value
-        else:
-            notunique.append(value)
     
-    return unique, notunique
+    return unique
 
-def count(line):
+def count_letters(line):
     ret = dict()
-    letters = ["a", "b", "c", "d", "e", "f", "g"]
 
-    for letter in letters:
-        for word in line:
-            for a in word:
-                if letter == a and a not in ret:
-                    ret[a] = 1
-                elif letter == a:
-                    ret[a] += 1
-
+    for value in line.split():
+        for letter in value:
+            if letter in ret:
+                ret[letter] += 1
+            else:
+                ret[letter] = 1
     return ret
 
-def find_nine(nonunique, counted):
-    a = None
-    for i in counted:
-        if counted[i] == 4:
-            a = i
-    for val in nonunique:
-        if len(val) == 6 and a not in val:
-            return val
+def find_six(line, unique):
+    for value in line.split():
+        if len(value) == 6:
+            for letter in unique[7]:
+                if letter not in value:
+                    return value
 
-def find_zero(nonunique):
-    for val in nonunique:
-        if len(val) == 6:
-            return val
+def find_zero(line, unique, six):
+    for value in line.split():
+        if len(value) == 6:
+            for letter in unique[4]:
+                if letter not in value and value != six:
+                    return value
 
-def find_two(nonunique, counted):
-    a = None
-    for i in counted:
-        if counted[i] == 4:
-            a = i
-
-    for l in nonunique:
-        if a in l:
-            return l
+def find_nine(line, unique, six, zero):
+    for value in line.split():
+        if len(value) == 6 and value not in (six, zero):
+            return value
 
 
-def find_five_three(nonunique, c):
+
+def find_three(line, unique):
+    for value in line.split():
+        p = 0
+        if len(value) == 5:
+            for letter in unique[1]:
+                if letter in value:
+                    p += 1
+        if p == 2:
+            return value
+
+def find_two_five(line, unique, three):
     five = None
-    three = None
-    for a in c:
-        if c[a] == 8:
-            for l in nonunique:
-                if a not in l:
-                    five = l
-                else:
-                    three = l
-    return five, three
-                    
+    two = None
+    for value in line.split():
+        if len(value) == 5 and value != three:
+            p = 0
+            for letter in unique[4]:
+                if letter not in value:
+                    p += 1
+            if p == 1:
+                five = value
+            elif p == 2:
+                two = value
 
-def find_six(unique, nonunique, counted):
-    a = None
-    for letter in unique[4]:
-        if  counted[letter] == 7:
-            a = letter
-    for val in nonunique:
-        if len(val) == 6 and a in val:
-            return val
+    return two, five
 
 
-def com(unique, nonunique, c):
-    nine = find_nine(nonunique, c)
-    nonunique.remove(nine)
-    six = find_six(unique, nonunique, c)
-    nonunique.remove(six)
-    zero = find_zero(nonunique)
-    nonunique.remove(zero)
-    two = find_two(nonunique, c)
-    nonunique.remove(two)
-    five, three = find_five_three(nonunique, c)
-
-    ret = unique
-    ret[5] = five
-    ret[6] = six
-    ret[0] = zero
-    ret[2] = two
-    ret[5] = five
-    ret[3] = three
-    ret[9] = nine
-
-    ret = {tuple(sorted(v)): k for k, v in ret.items()}
-
-    return ret
-
-def calculate(parsed):
-    summ = []
-    for line in parsed:
-        unique, nonunique = first_pass(line[0].split())
-        c = count(line[0].split())
-        ret = ""
-        final = com(unique, nonunique, c)
-        print(final)
-        for a in line[1].split():
-            if tuple(sorted(a)) in final:
-                ret += str(final[tuple(sorted(a))])
-
-        summ.append(int(ret))
-
-    print(summ)
+def calc(inp):
+    total = 0
+    for line in inp:
+        unique = get_unique(line)
+        six = find_six(line[0], unique)
+        zero = find_zero(line[0], unique, six)
+        nine = find_nine(line[0], unique, six, zero)
+        three = find_three(line[0], unique)
+        two, five = find_two_five(line[0], unique, three) 
 
 
+        table = dict()
 
+        for i in unique:
+            table[tuple(sorted(unique[i]))] = i
 
+        table[tuple(sorted(six))] = 6
+        table[tuple(sorted(zero))] = 0
+        table[tuple(sorted(nine))] = 9
+        table[tuple(sorted(five))] = 5
+        table[tuple(sorted(three))] = 3
+        table[tuple(sorted(two))] = 2
+        f = ""
+        for pot in line[1].split():
+            if tuple(sorted(pot)) in table:
+                f += str(table[tuple(sorted(pot))])
+        total += int(f)
 
-
-def main(parsed):
-    calculate(parsed)
-
+    return total
 
 if __name__=="__main__":
-    parsed = get_input()
-    main(parsed)
+    inp = get_input()
+    result = calc(inp)
+    print(result)
+
