dc8ad1333f4079d297e5ce068f9e1b01c9542beb
enticies <enticies@protonmail.com>
2024-12-02 11:07:58 +0400

    Day, 2024.

--- 2024/2day/solution.go
+++ 2024/2day/solution.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
-	"sort"
+	"math"
 	"strconv"
 	"strings"
 )
@@ -18,55 +18,91 @@ func main() {
 		log.Fatalf("Failed to read the file: %v", err)
 	}
 
-	firstColumn, secondColumn := parseInput(content)
+	rows := parseInput(content)
 
-	partOneResult := partOneSolution(firstColumn, secondColumn)
-	partTwoResult := partTwoSolution(firstColumn, secondColumn)
+	partOneResult := partOneSolution(rows)
+	partTwoResult := partTwoSolution(rows)
 
 	fmt.Printf("Part one result is %d\n", partOneResult)
 	fmt.Printf("Part two result is %d\n", partTwoResult)
 }
 
-func parseInput(content []byte) ([]int, []int) {
+func parseInput(content []byte) [][]int {
 	lines := strings.Split(strings.TrimSpace(string(content)), "\n")
 
-	firstColumn := make([]int, 0)
-	secondColumn := make([]int, 0)
+	rows := make([][]int, 0)
 
 	for _, line := range lines {
 		splitLine := strings.Fields(line)
 
-		firstNum, err := strconv.Atoi(splitLine[0])
+		numberLine := make([]int, 0)
 
-		if err != nil {
-			panic(err)
+		for _, val := range splitLine {
+			num, err := strconv.Atoi(val)
+
+			if err != nil {
+				panic(err)
+			}
+
+			numberLine = append(numberLine, num)
 		}
 
-		secondNum, err := strconv.Atoi(splitLine[1])
-
-		if err != nil {
-			panic(err)
-		}
-
-		firstColumn = append(firstColumn, firstNum)
-		secondColumn = append(secondColumn, secondNum)
+		rows = append(rows, numberLine)
 	}
 
-	sort.Ints(firstColumn)
-	sort.Ints(secondColumn)
-
-
-	return firstColumn, secondColumn
+	return rows
 }
 
-func partOneSolution(firstColumn []int, secondColumn []int) int {
-	result := 0 
-
-	return result
+func partOneSolution(rows [][]int) int {
+	unsafeLevels := 0
+	for _, row := range rows {
+		if !isRowSafe(row) {
+			unsafeLevels++
+		}
+	}
+	return len(rows) - unsafeLevels
 }
 
-func partTwoSolution(firstColumn []int, secondColumn []int) int {
-	result := 0
+func partTwoSolution(rows [][]int) int {
+	safeLevels := 0
+	for _, row := range rows {
+		if isRowSafe(row) {
+			safeLevels++
+			fmt.Println(row, "- SAFE")
+		} else if isSafeByRemoving(row) {
+			safeLevels++
+		} else {
+			fmt.Println(row, "- UNSAFE")
+		}
+	}
+	return safeLevels
+}
 
-	return result
+func isSafeByRemoving(row []int) bool {
+	for j := 0; j < len(row); j++ {
+		newRow := make([]int, 0, len(row)-1)
+		newRow = append(newRow, row[:j]...)
+		newRow = append(newRow, row[j+1:]...)
+
+		if isRowSafe(newRow) {
+			fmt.Println(row, " SAFE BY REMOVING ", row[j])
+			return true
+		}
+	}
+	return false
+}
+
+func isRowSafe(row []int) bool {
+	ascending := false
+
+	if row[0] < row[1] {
+		ascending = true
+	}
+
+	for i := 1; i < len(row); i++ {
+		if (row[i-1] <= row[i] && !ascending) || (row[i-1] >= row[i] && ascending) || math.Abs(float64(row[i-1])-float64(row[i])) > 3 || math.Abs(float64(row[i-1])-float64(row[i])) < 1 {
+			return false
+		}
+	}
+	return true
 }
